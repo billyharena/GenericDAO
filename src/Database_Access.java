1c2023a2ef34763ca9aa198aa53b189f0aa1fb8b
billyharena <billy.harena@gmail.com>
2022-12-15 05:36:26 +0300

    Database_Access creation
    Creation of the class Database_Access which will manage the connexion to the database

---
+++ src/Database_Access.java
@@ -0,0 +1,58 @@
+import java.sql.*;
+
+
+public class Database_Access {
+    private Connection cnt;
+    private Statement stmt;
+    private ResultSet rs;
+
+    // Open a postgreSQL connection
+    public Connection getPostgreSQLConnexion(String user, String databasename, String password, String port) {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.cnt = DriverManager.getConnection("jdbc:postgresql://localhost:" + port + "/" + databasename, user, password);
+            if (cnt != null) System.out.println("Connected in PostgreSQL database = " +databasename );
+            else System.out.println("Connection failed");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cnt;
+    }
+
+    // Close the statement
+    public void closeStatement(){
+        if (stmt != null){
+            try {
+                stmt.close();
+                System.out.println("Statement closed");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // Close the resultset
+    public void closeResultSet(){
+        if (rs != null){
+            try {
+                rs.close();
+                System.out.println("ResultSet closed");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    // Close the connection
+    public void closeConnection(){
+        if (cnt != null){
+            try {
+                cnt.close();
+                System.out.println("Connection closed");
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Database_Access da = new Database_Access();
+        String user = "tahina";
+        String password = "tahina";
+        String databasename = "extraction";
+        String port = "5432";
+        da.getPostgreSQLConnexion(user, databasename, password, port);
+        da.closeConnection();
     }
 }
